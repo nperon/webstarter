33b607ce0cbf9344dce749fdafd2aceb155e7165
Nicolas Peron <nperon@gmail.com>
2020-08-21 23:10:31 +0200

    Terser plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -41,5 +42,8 @@ module.exports = {
                 },
             }
         ]
-    }
+    },
+    plugins: [
+        new TerserPlugin()
+    ]
 }
